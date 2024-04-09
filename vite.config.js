4f9c40a46e2b1e029aeaf6efce4635522e8bed77
kore <emily@cat-girl.gay>
2024-04-09 12:50:32 -0400

    uhhhhhh

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/wiktionary-intersection/',
+  base: '/wiktionary-intersection/',
   plugins: [
     vue(),
     wasm(),
